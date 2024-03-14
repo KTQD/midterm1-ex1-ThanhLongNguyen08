f946e4c51a65ddcdee076dba16c98c63e9c1bed7
ThanhLongNguyen08 <152238351+ThanhLongNguyen08@users.noreply.github.com>
2024-03-14 08:03:02 +0700

    Update MP3.java

--- src/MP3.java
+++ src/MP3.java
@@ -1,2 +1,14 @@
 public class MP3 {
+      private Duration duration;
+    public MP3 (String name, String description, String ID, int price, Duration duration) {
+        super(name,description,ID,price);
+        this.duration = duration;
+    }
+
+
+    public void showInfoMp3() {
+        super.showInfo();
+        System.out.println("price: "+price);
+        System.out.println("duration: "+duration);
+    }
 }
