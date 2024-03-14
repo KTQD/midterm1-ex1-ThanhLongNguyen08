d8f42d86d2f16b3503c2d83c3d68b725f01f426f
ThanhLongNguyen08 <152238351+ThanhLongNguyen08@users.noreply.github.com>
2024-03-14 08:02:23 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,28 @@
 public class Item {
+      public String name;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String description;
+    protected String ID;
+    int price;
+
+    public void showInfo(){
+        System.out.println("name: "+this.name);
+        System.out.println("description: "+this.getDescription());
+        System.out.println("ID: "+this.ID);
+    }
+
+    public Item(String name, String description, String ID, int price) {
+        this.name = name;
+        this.description = description;
+        this.ID = ID;
+        this.price = price;
+    }
 }
